f91b728f201513af963f9107b97af22335c92127
Mike <MikeFvv@gmail.com>
2018-07-20 13:10:21 +0800

    初始化 添加以前的

---
+++ KKKWemb/KKKWemb/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  KKKWemb
+//
+//  Created by mike zhang on 2018/7/20.
+//  Copyright © 2018年 mike zhang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
